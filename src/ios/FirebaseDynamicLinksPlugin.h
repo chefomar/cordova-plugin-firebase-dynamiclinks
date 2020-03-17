1d07ef5ec13a06270936b0d836bd829b86d3e0a4
chefomar <elshinnaweyom@gmail.com>
2020-03-17 10:02:16 +0300

    remove stuff not used

--- src/ios/FirebaseDynamicLinksPlugin.h
+++ src/ios/FirebaseDynamicLinksPlugin.h
@@ -6,10 +6,8 @@
 @interface FirebaseDynamicLinksPlugin : CDVPlugin
 
 - (void)onDynamicLink:(CDVInvokedUrlCommand *)command;
-- (void)createDynamicLink:(CDVInvokedUrlCommand *)command;
 - (void)postDynamicLink:(FIRDynamicLink*) dynamicLink;
 
-@property (nonatomic, copy) NSString* domainUriPrefix;
 @property (nonatomic, copy) NSString* dynamicLinkCallbackId;
 @property (nonatomic, retain) NSDictionary* lastDynamicLinkData;
 
